62f3a9b6618663db913e240b77de9e69f5bf4ce9
BenKaestle <44409702+BenKaestle@users.noreply.github.com>
2019-11-17 13:26:17 +0100

    stuff

--- main/src/phylotree/Main.java
+++ main/src/phylotree/Main.java
@@ -1,39 +1,47 @@
 package phylotree;
 
-import java.io.File;
-import java.io.IOException;
+import mash.MashDistance;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) throws IOException {
 //        System.out.println(ParseRefSeq.getAllCompleteGenomes(args[1]).size());
 //        downloadFromYear(Integer.parseInt(args[0]), ParseRefSeq.getAllCompleteGenomes(args[1]));
 
-        applyMash(args[0],args[1]);
+        applyMash(args[0]);
     }
 
-    private static void applyMash(String filepath, String mashfilepath){
-        ArrayList<String> files = getAllFilesFromDir(filepath);
-        String command = mashfilepath +" sketch ";
-        for (String s : files){
-            command.concat(s+" ");
-        }
-        command.concat("-o "+filepath+"/sketchout -p 100");
-
+    private static void applyMash(String filepath) throws IOException {
         Process p = null;
         try {
-            p = Runtime.getRuntime().exec(command);
+            p = Runtime.getRuntime().exec("/home/kaestle/Mash/mash-Linux64-v2.2/mash dist "+filepath+" "+filepath+" -p 100\n");
             p.waitFor();
-        } catch (IOException | InterruptedException e) {
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
+        BufferedReader buf = new BufferedReader(new InputStreamReader(
+                p.getInputStream()));
+        String line = "";
+
+
+        FileWriter fileWriter = null;
+
+        fileWriter = new FileWriter(filepath+"_out.txt");
+
+        while ((line = buf.readLine()) != null) {
+            fileWriter.write(line + "\n");
+        }
+        fileWriter.close();
+
 
     }
 
-    public static ArrayList<String> getAllFilesFromDir (String filepath){
+    public static ArrayList<String> getAllFilesFromDir(String filepath) {
         final File folder = new File(filepath);
 
         ArrayList<String> result = new ArrayList<>();
@@ -58,23 +66,24 @@ public class Main {
 
         }
     }
-    public static void downloadFromYear(int year, ArrayList<CompleteGenome> genomes){
+
+    public static void downloadFromYear(int year, ArrayList<CompleteGenome> genomes) {
         Process p, q;
-        int count=0;
+        int count = 0;
         for (CompleteGenome genome : genomes) {
             if (genome.year == year) {
                 count++;
             }
         }
-        int counter=0;
-        for (CompleteGenome genome : genomes){
-            if (genome.year == year){
+        int counter = 0;
+        for (CompleteGenome genome : genomes) {
+            if (genome.year == year) {
                 try {
                     counter++;
-                    System.out.println(counter + " / "+ count);
-                    p = Runtime.getRuntime().exec("wget "+genome.downloadLink);
+                    System.out.println(counter + " / " + count);
+                    p = Runtime.getRuntime().exec("wget " + genome.downloadLink);
                     p.waitFor();
-                    q = Runtime.getRuntime().exec("gunzip "+genome.downloadLink);
+                    q = Runtime.getRuntime().exec("gunzip " + genome.downloadLink);
                     q.waitFor();
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
